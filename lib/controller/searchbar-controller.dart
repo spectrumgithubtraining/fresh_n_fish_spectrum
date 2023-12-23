e7c6b341ea19aa6f8fce69f9fcdbf824aaac8a68
Your Name <spectrumgithubtraining>
2023-12-23 14:54:42 +0530

    done

--- lib/controller/searchbar-controller.dart
+++ lib/controller/searchbar-controller.dart
@@ -1,15 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+import '../models/product-model.dart';
 
 class SearchBarController extends GetxController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<List<QueryDocumentSnapshot<Object?>>> getSearchData(
-      String uId, String query) async {
+  RxList<ProductModel> searchResults = <ProductModel>[].obs;
+
+  Future<void> getSearchData(String query) async {
     final QuerySnapshot productData = await _firestore
         .collection('products')
-        .where('categoryId', isEqualTo: query)
+        .where('productName', isGreaterThanOrEqualTo: query)
+        .where('productName', isLessThan: query + 'z')
         .get();
-    return productData.docs;
+
+    searchResults.assignAll(productData.docs
+        .map((doc) => ProductModel.fromMap(doc.data() as Map<String, dynamic>))
+        .toList());
   }
 }
