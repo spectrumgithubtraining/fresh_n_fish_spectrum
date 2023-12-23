c510df1a293521c6b08c7eb1cb1000522e691ac9
Your Name <spectrumgithubtraining>
2023-12-23 09:15:25 +0530

    done

---
+++ lib/view/auth_ui/sign-up-screen.dart
@@ -0,0 +1,295 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fresh_n_fish_spectrum/view/auth_ui/sentopt.dart';
+import 'package:fresh_n_fish_spectrum/view/auth_ui/welcome_screen.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+
+import '../../controller/email-sign-in-controller.dart';
+import '../../controller/google-sign-in-controller.dart';
+import '../../services/validator/validator.dart';
+import '../../utils/app-constant.dart';
+import 'email_validation.dart';
+import 'forgot_password_screen.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameTextController = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  final GoogleSignInController _googleSignInController =
+      Get.put(GoogleSignInController());
+  final EmailPassController _emailPassController =
+      Get.put(EmailPassController());
+  bool passwordVisible = false;
+  Widget getTextField(
+      {required String hint,
+      required var icons,
+      bool obstxt = false,
+      var suficons,
+      required var validator,
+      required var controller,
+      required var keyboardType}) {
+    return TextFormField(
+      obscureText: obstxt,
+      keyboardType: keyboardType,
+      validator: validator,
+      controller: controller,
+      decoration: InputDecoration(
+          suffixIcon: suficons,
+          errorStyle: const TextStyle(
+            color: Colors.yellow,
+            fontSize: null,
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.normal,
+          ),
+          prefixIcon: icons,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.r),
+            borderSide: const BorderSide(color: Colors.transparent),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.r),
+            borderSide: const BorderSide(color: Colors.transparent),
+          ),
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+          filled: true,
+          fillColor: const Color(0xFFF1F4FF),
+          hintText: hint,
+          hintStyle: TextStyle(
+            color: Colors.black54,
+            fontFamily: 'Roboto-Regular',
+            fontSize: 15.sp,
+            height: 0.h,
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: AppConstant.appScendoryColor,
+        appBar: AppBar(
+          backgroundColor: AppConstant.appScendoryColor,
+          elevation: 0,
+          leading: IconButton(
+              onPressed: () => Get.offAll(() => const WelcomeScreen(),
+                  transition: Transition.leftToRightWithFade),
+              icon: const Icon(CupertinoIcons.back, color: Colors.white)),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: Get.width,
+            height: Get.height,
+            child: Column(children: [
+              Container(
+                margin: const EdgeInsets.only(top: 70.0).w,
+                child: Column(children: [
+                  Text(
+                    'Create Account',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppConstant.yellowText,
+                      fontSize: 30.sp,
+                      fontFamily: 'Roboto-Bold',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  const Text(
+                    'Create an account so you can \n explore all the existing jobs',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ]),
+              ),
+              Expanded(
+                child: Center(
+                  child: Form(
+                    key: _formKey,
+                    child: SizedBox(
+                      width: 323.w,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          getTextField(
+                              keyboardType: TextInputType.text,
+                              hint: "Name",
+                              icons: const Icon(Icons.person_outline),
+                              validator: (value) => Validator.validateName(
+                                    name: value,
+                                  ),
+                              controller: _nameTextController),
+                          SizedBox(
+                            height: 26.h,
+                          ),
+                          getTextField(
+                              keyboardType: TextInputType.emailAddress,
+                              hint: "Email",
+                              icons: const Icon(Icons.email),
+                              validator: (value) => Validator.validateEmail(
+                                    email: value,
+                                  ),
+                              controller: _emailTextController),
+                          SizedBox(
+                            height: 26.h,
+                          ),
+                          getTextField(
+                              obstxt: passwordVisible,
+                              suficons: IconButton(
+                                  onPressed: () {
+                                    setState(
+                                      () {
+                                        passwordVisible = !passwordVisible;
+                                      },
+                                    );
+                                  },
+                                  icon: Icon(passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off)),
+                              keyboardType: TextInputType.visiblePassword,
+                              hint: "Password",
+                              icons: const Icon(Icons.lock),
+                              validator: (value) => Validator.validatePassword(
+                                    password: value,
+                                  ),
+                              controller: _passwordTextController),
+                          SizedBox(
+                            height: 15.h,
+                          ),
+                          Container(
+                            alignment: Alignment.topRight,
+                            child: TextButton(
+                              onPressed: () {
+                                Get.offAll(() => const ForgotPasswordPage(),
+                                    transition: Transition.leftToRightWithFade);
+                              },
+                              child: Text(
+                                'Forgot your password?',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontFamily: 'Roboto-Regular',
+                                  color: AppConstant.appTextColor,
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.w600,
+                                  height: 0.h,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15.h,
+                          ),
+                          SizedBox(
+                              width: 357.w,
+                              height: 50.h,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(9.r))),
+                                    backgroundColor:
+                                        const MaterialStatePropertyAll(
+                                            Color(0xFF1F41BB))),
+                                onPressed: () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    try {
+                                      await _emailPassController.signupUser(
+                                        _emailTextController.text,
+                                        _passwordTextController.text,
+                                        _nameTextController.text,
+                                      );
+                                      if (_emailPassController.currentUser !=
+                                          null) {
+                                        Get.off(
+                                            () => EmailValidationScreen(
+                                                user: _emailPassController
+                                                    .currentUser!),
+                                            transition:
+                                                Transition.leftToRightWithFade);
+                                      } else {
+                                        // No user is currently authenticated
+                                        Get.snackbar('No user is',
+                                            'currently authenticated');
+                                      }
+                                    } catch (e) {
+                                      Get.snackbar('Error', e.toString());
+                                    }
+                                  }
+                                },
+                                child: Text(
+                                  'Sign Up',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: AppConstant.appTextColor,
+                                    fontSize: 20.sp,
+                                    height: 0.h,
+                                    fontFamily: 'Roboto-Bold',
+                                  ),
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(bottom: 90.0).w,
+                width: MediaQuery.of(context).size.width,
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  GestureDetector(
+                    onTap: () {
+                      _googleSignInController.signInWithGoogle();
+                    },
+                    child: SizedBox(
+                      width: 60.w,
+                      height: 44.h,
+                      child: SvgPicture.asset(
+                          'assets/images/flat-color-icons_google.svg'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  GestureDetector(
+                    onTap: () => Get.to(() => const SendOtp()),
+                    child: SizedBox(
+                      width: 60.w,
+                      height: 44.h,
+                      child: SvgPicture.asset(
+                          'assets/images/ic_sharp-local-phone.svg'),
+                    ),
+                  ),
+                ]),
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/screens/product-deatils-screen.dart
@@ -0,0 +1,234 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../controller/get-cart-product-controller.dart';
+import '../../models/product-model.dart';
+
+import '../../utils/app-constant.dart';
+import '../main-page.dart';
+
+class ProductDetailsScreen extends StatefulWidget {
+  ProductModel productModel;
+  ProductDetailsScreen({super.key, required this.productModel});
+
+  @override
+  State<ProductDetailsScreen> createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+  final CartItemController _CartItemController = Get.put(CartItemController());
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: AppConstant.appTextColor),
+          backgroundColor: AppConstant.appMainColor,
+          centerTitle: true,
+          title: Text("Products details page",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.sp,
+                fontFamily: 'Roboto-Regular',
+              )),
+          leading: IconButton(
+              onPressed: () => Get.offAll(() => const MainPage(),
+                  transition: Transition.leftToRightWithFade),
+              icon: const Icon(CupertinoIcons.back, color: Colors.white)),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          width: Get.width,
+          height: Get.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //product images
+
+              SizedBox(
+                height: Get.height / 60,
+              ),
+              CarouselSlider(
+                items: widget.productModel.productImages
+                    .map(
+                      (imageUrls) => ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: CachedNetworkImage(
+                          imageUrl: imageUrls,
+                          fit: BoxFit.cover,
+                          width: Get.width - 10,
+                          placeholder: (context, url) => const ColoredBox(
+                            color: Colors.white,
+                            child: Center(
+                              child: CupertinoActivityIndicator(),
+                            ),
+                          ),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                options: CarouselOptions(
+                  scrollDirection: Axis.horizontal,
+                  autoPlay: true,
+                  aspectRatio: 2.5,
+                  viewportFraction: 1,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(widget.productModel.productName,
+                                  style: TextStyle(
+                                    color: AppConstant.appMainColor,
+                                    fontSize: 18.sp,
+                                    fontFamily: 'Roboto-Bold',
+                                  )),
+                              const Icon(Icons.favorite_outline)
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Row(
+                            children: [
+                              widget.productModel.isSale == true &&
+                                      widget.productModel.salePrice != ''
+                                  ? Text(
+                                      "Price : ₹ " +
+                                          widget.productModel.salePrice,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,
+                                        fontFamily: 'Roboto-Bold',
+                                      ))
+                                  : Text(
+                                      "Price : ₹ " +
+                                          widget.productModel.fullPrice,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15.sp,
+                                        fontFamily: 'Roboto-Bold',
+                                      )),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                              "Category : " + widget.productModel.categoryName,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13.sp,
+                                fontFamily: 'Roboto-Regular',
+                              )),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(widget.productModel.productDescription,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13.sp,
+                                fontFamily: 'Roboto-Regular',
+                              )),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Material(
+                              child: Container(
+                                width: Get.width / 3.0,
+                                height: Get.height / 16,
+                                decoration: BoxDecoration(
+                                  color: AppConstant.appScendoryColor,
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: TextButton(
+                                  child: Text("WhatsApp",
+                                      style: TextStyle(
+                                        color: AppConstant.appTextColor,
+                                        fontSize: 13.sp,
+                                        fontFamily: 'Roboto-Regular',
+                                      )),
+                                  onPressed: () {
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5.0,
+                            ),
+                            Material(
+                              child: Container(
+                                width: Get.width / 3.0,
+                                height: Get.height / 16,
+                                decoration: BoxDecoration(
+                                  color: AppConstant.appScendoryColor,
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: TextButton(
+                                  child: const Text(
+                                    "Add to cart",
+                                    style: TextStyle(
+                                        color: AppConstant.appTextColor),
+                                  ),
+                                  onPressed: () async {
+                                    // Get.to(() => SignInScreen());
+                                    await _CartItemController
+                                        .checkProductExistence(
+                                            uId: user!.uid,
+                                            productModel: widget.productModel);
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
